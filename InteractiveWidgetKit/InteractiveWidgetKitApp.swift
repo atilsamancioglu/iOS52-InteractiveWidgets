5e010baff8580137555e6610b3eab99965a2ff0d
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-08-19 16:10:47 +0300

    Initial Commit

---
+++ InteractiveWidgetKit/InteractiveWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetKitApp.swift
+//  InteractiveWidgetKit
+//
+//  Created by Atil Samancioglu on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
